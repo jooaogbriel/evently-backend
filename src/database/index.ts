88d3a37a24922e1ce7675fd0ea02252d5023d865
João Gonçalves <joao_i0807@outlook.com>
2024-02-11 10:26:17 -0300

    fix: ajustando conexao banco de dados

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,18 @@
 const { MongoClient } = require("mongodb");
-
-// Replace the uri string with your connection string.
-const uri = process.env.MONGODB_URI;
-
-const client = new MongoClient(uri);
-
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = 'mongodb+srv://joao656217:1234@cluster0.tezi9s6.mongodb.net/?retryWrites=true&w=majority'
+// Connect to your Atlas cluster
+const client = new MongoClient(url);
 async function run() {
-  try {
-    const database = client.db('real_time');
-    const movies = database.collection('movies');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+    } catch (error) {
+        console.error('deu erro');
+    }
+    finally {
+        await client.close();
+    }
 }
 run().catch(console.dir);
