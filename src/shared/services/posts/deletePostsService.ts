b661f62bacff451c812e10b00f4b0b96ff8a4173
João Gonçalves <joao_i0807@outlook.com>
2024-02-28 23:40:30 -0300

    fix: delecao de post feita para a model de user também

--- src/shared/services/posts/deletePostsService.ts
+++ src/shared/services/posts/deletePostsService.ts
@@ -13,6 +13,10 @@ const deletePostsService =  async(idPost: string, idUser: string) => {
             throw new Error('Você não tem permissão para excluir este post.');
         }
         const deletedPost = await Post.findByIdAndDelete(idPost);
+        const updateUserResult = await User.updateOne({ _id: idUser }, { $pull: { posts: { _id: idPost } } });
+        console.log('Update user result:', updateUserResult);
+        const userBeforeUpdate = await User.findById(idUser);
+console.log('User before update:', userBeforeUpdate);
         return deletedPost;
 
     } catch(err) {
